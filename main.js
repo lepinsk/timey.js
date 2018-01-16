fe61685daaa7872cadd057ab9f5847b19e94f732
Julian Lepinski <lepinski@gmail.com>
2018-01-16 15:55:58 -0500

    main.js: adding path to go from ms → human readable

--- main.js
+++ main.js
@@ -7,7 +7,25 @@ if (process.argv.length === 3 && process.argv[0].indexOf("node") !== -1) {
 
 if (process.argv.length === 2) {
 	var timeIn = process.argv[1];
-	var timeInOrig = timeIn;
+	if (timeIn.indexOf(":") !== -1 || timeIn.indexOf(".") !== -1) {
+		var timeInOrig = timeIn;
+		var timeInMS = processHumanTime(timeIn);
+		console.log(timeInOrig + " in milliseconds is " + timeInMS);
+	} else {
+		var timeInOrig = timeIn;
+		var timeInHuman = processMilliseconds(timeIn);
+		console.log(timeInOrig + "ms in human time is " + timeInHuman);
+	}
+} else {
+	console.log("whoops: timey expects a single argument, i.e.");
+	console.log("");
+	console.log("   timey 01:02:03");
+	console.log("or");
+	console.log("   timey 230000");
+	process.exit(1);
+}
+
+function processHumanTime(timeIn) {
 	var msComponent = 0;
 	var sComponent = 0;
 	var mComponent = 0;
@@ -28,10 +46,31 @@ if (process.argv.length === 2) {
 	}
 	var timeInMS = (hComponent * 60 * 60000) + (mComponent * 60000) + (sComponent * 1000) + msComponent;
 	timeInMS = parseInt(timeInMS);
-	console.log(timeInOrig + " in milliseconds is " + timeInMS);
-} else {
-	console.log("error: I expect a single argument, i.e.");
-	console.log("");
-	console.log("   timey 01:02:03");
-	process.exit(1);
+	return timeInMS;
+}
+
+function processMilliseconds(msIn) {
+	var hComponent = ~~(msIn / (60 * 60000));
+	msIn -= hComponent * (60 * 60000);
+	var mComponent = ~~(msIn / 60000);
+	msIn -= mComponent * 60000;
+	var sComponent = ~~(msIn / 1000);
+	var msComponent = msIn % 1000;
+	var humanReadable = 	pad2(hComponent) + ":" + 
+							pad2(mComponent) + ":" +
+							pad2(sComponent) + "." +
+							padToDigits(msComponent, 3);
+	return humanReadable;
+}
+
+function pad2(numberIn) {
+	return padToDigits(numberIn, 2);
+}
+
+function padToDigits(numberIn, digits) {
+	numberIn = "" + numberIn;
+	while (numberIn.length < digits) {
+		numberIn = "0" + numberIn;
+	}
+	return numberIn;
 }
